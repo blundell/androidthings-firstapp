b7077eae3386cb8314c1a581080b3858ad01e70d
Blundell <blundell.app.dev@gmail.com>
2017-08-02 22:10:02 +0100

    Initial Commit :rocket:

---
+++ app/src/main/java/com/blundell/firstapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.blundell.firstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("TUT", "Hello AndroidThings World");
+    }
+}
